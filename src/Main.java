27f7ac3603ffc67591d43a5bd7d2858a1fe613c6
Paclocho <120619725+Paclocho@users.noreply.github.com>
2022-12-15 20:39:02 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int total;
+        total = suma3(5, 10, 1);
+        System.out.println(total);
+    }
+
+    public static int suma3(int a, int b, int c) {
+        return a + b + c;
+    }
+}
